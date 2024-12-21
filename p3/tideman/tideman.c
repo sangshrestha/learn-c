4116591a0ca187c05d424e81f290f7c503dbfd6b
sangshrestha <sang.socials@gmail.com>
2024-12-21 20:34:38 +0000

    feat: finish tideman

--- p3/tideman/tideman.c
+++ p3/tideman/tideman.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int sort(int values[], int length);
-
 #define MAX_CANDIDATES 9
 #define MAX_VOTERS 9999
 #define RANK_COUNT 3
 #define INT_SIZE 32
 #define NAME_LENGTH 30
+
+int sort(int values[], int length);
+int check_cycle(int graph[][MAX_CANDIDATES], int i, int j, int length);
 
 typedef struct
 {
@@ -18,6 +19,7 @@ typedef struct
 
 candidate candidates[MAX_CANDIDATES];
 int preferences[MAX_CANDIDATES][MAX_CANDIDATES];
+int graph[MAX_CANDIDATES][MAX_CANDIDATES];
 
 int main(int argc, char *argv[])
 {
@@ -105,8 +107,6 @@ int main(int argc, char *argv[])
                 }
             }
         }
-
-        printf("\n");
     }
 
     // Printing preferences matrix
@@ -124,6 +124,7 @@ int main(int argc, char *argv[])
     int pair_count = 0;
     int pairs[MAX_CANDIDATES][2];
     int pair_scores[MAX_CANDIDATES];
+    int sorted_pairs[MAX_CANDIDATES][2];
 
     for (int i = 0; i < argc - 1; i++)
     {
@@ -164,13 +165,126 @@ int main(int argc, char *argv[])
 
     sort(pair_scores, pair_count);
 
-    printf("\nSorted pairs:\n");
+    // Print sorted values
+    printf("\nScores sorted:\n");
     for (int i = 0; i < pair_count; i++)
     {
-        printf("%d, ", pair_scores[i]);
+        printf("%d ", pair_scores[i]);
+    }
+    printf("\n");
+
+    // Assign to sorted_pairs
+    int sorted_index = 0;
+    for (int i = 0; i < pair_count; i++)
+    {
+        for (int j = 0; j < pair_count; j++)
+        {
+            int source_index = pairs[j][0];
+            int target_index = pairs[j][1];
+            int score = preferences[source_index][target_index];
+
+            if (score == pair_scores[i])
+            {
+                sorted_pairs[sorted_index][0] = source_index;
+                sorted_pairs[sorted_index][1] = target_index;
+                sorted_index++;
+            }
+        }
+    }
+
+    // Print sorted pairs
+    printf("\nPairs sorted:\n");
+    for (int i = 0; i < pair_count; i++)
+    {
+        int source_index = sorted_pairs[i][0];
+        int target_index = sorted_pairs[i][1];
+        printf("%d, %d: %d\n", source_index, target_index, preferences[source_index][target_index]);
+    }
+
+    // Add edges to graph
+    printf("\n");
+    for (int i = 0; i < pair_count; i++)
+    {
+        if (check_cycle(graph, sorted_pairs[i][0], sorted_pairs[i][1], pair_count) == 0)
+        {
+            graph[sorted_pairs[i][0]][sorted_pairs[i][1]] = 1;
+        }
+        else
+        {
+            printf("Cycle found: %d, %d\n", sorted_pairs[i][0], sorted_pairs[i][1]);
+        }
+    }
+
+    // Printing graph
+    printf("\nGraph:\n");
+    for (int i = 0; i < argc - 1; i++)
+    {
+        for (int j = 0; j < argc - 1; j++)
+        {
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Get winner - finding 0 column
+    int search_index = 0;
+    int winner_flag = 0;
+
+    while (winner_flag == 0 && search_index < pair_count)
+    {
+        int edge_flag = 0;
+
+        for (int i = 0; i < pair_count; i++)
+        {
+            if (graph[i][search_index] == 1)
+            {
+                edge_flag = 1;
+            }
+        }
+
+        if (edge_flag == 0)
+        {
+            winner_flag = 1;
+        }
+        else
+        {
+            search_index++;
+        }
+    }
+
+    printf("\n");
+    if (winner_flag == 1)
+    {
+        printf("Winner: %s\n", candidates[search_index].name);
+    }
+    else
+    {
+        printf("No winner\n");
     }
 }
 
+// Check recursively if adding an edge at i,j creates a cycle
+int check_cycle(int graph[][MAX_CANDIDATES], int i, int j, int length)
+{
+    for (int k = 0; k < length; k++)
+    {
+        if (graph[j][k] == 1)
+        {
+            if (k == i)
+            {
+                return 1;
+            }
+            else
+            {
+                return check_cycle(graph, i, k, length);
+            }
+        }
+    }
+
+    return 0;
+}
+
+// Bubble sort algorithm to sort array in place
 int sort(int values[], int length)
 {
     for (int i = 0; i < length; i++)
@@ -179,6 +293,7 @@ int sort(int values[], int length)
 
         for (int j = 0; j < length - 1; j++)
         {
+            // < sorts from greater to lower
             if (values[j] < values[j + 1])
             {
                 int temp = values[j];
